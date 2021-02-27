efdf6b92551ecbd42b5ba6863162b94cf3b3b859
Selin Güzel <76588842+selinguz@users.noreply.github.com>
2021-02-28 00:50:47 +0300

    Add files via upload

---
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using ConsoleUI;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+	public class CarManager : ICarService
+	{
+		ICarDal _iCarDal;
+
+		public CarManager(ICarDal carDal)
+		{
+			_iCarDal = carDal;
+		}
+
+		public List<Car> GetAll()
+		{
+			return _iCarDal.GetAll();
+		}
+	}
+}
