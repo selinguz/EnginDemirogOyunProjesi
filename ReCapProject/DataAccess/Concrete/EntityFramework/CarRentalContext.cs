78ea1cfd71f2c2afd41d0b8d13e93fd7b81462df
Selin Güzel <76588842+selinguz@users.noreply.github.com>
2021-03-04 23:10:04 +0300

    IEntityRepository altyapısı hazırlandı
    8. gün ödevi tamamlandı

---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/CarRentalContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleUI;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class CarRentalContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-5VST9JT\SQLEXPRESS; Database=CarRental;Trusted_connection=true");
+		}
+
+		public DbSet<Car> Car { get; set; }
+		public DbSet<Brand> Brand { get; set; }
+		public DbSet<Color> Color { get; set; }
+
+	}
+}
